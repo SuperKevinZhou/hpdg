13af4b04907e5ea30dd09e1e398c90830ce6445e
SuperKevinZhou <zzh__2020@outlook.com>
2025-08-21 16:21:35 +0800

    Add is_pal_string and is_pal_u64 in math module; Add and pass their tests

--- src/math.rs
+++ src/math.rs
@@ -33,6 +33,54 @@ pub fn is_perm(a: u64, b: u64) -> bool {
     true
 }
 
+pub fn is_pal_string(s: String) -> bool {
+    if s.is_ascii() {
+        let bytes = s.as_bytes();
+        let mut left = 0;
+        let mut right = bytes.len().saturating_sub(1);
+        while left < right {
+            if bytes[left] != bytes[right] {
+                return false;
+            }
+            left += 1;
+            right -= 1;
+        }
+        true
+    } else {
+        let mut forward = s.char_indices();
+        let mut backward = s.char_indices().rev();
+        
+        while let (Some((i, f_char)), Some((j, b_char))) = (forward.next(), backward.next()) {
+            if i >= j {
+                break;
+            }
+            if f_char != b_char {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+pub fn is_pal_u64(n: u64) -> bool {
+    if n == 0 {
+        return true;
+    }
+    if n % 10 == 0 {
+        return false;
+    }
+
+    let mut x = n;
+    let mut reversed = 0;
+
+    while x > reversed {
+        reversed = reversed * 10 + x % 10;
+        x /= 10;
+    }
+
+    x == reversed || x == reversed / 10
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,5 +93,40 @@ mod tests {
         assert_eq!(is_perm(1222, 2221), true);
         assert_eq!(is_perm(222, 222), true);
         assert_eq!(is_perm(222, 2222), false);
+    }
+
+    #[test]
+    fn test_is_pal_u64() {
+        assert_eq!(is_pal_u64(0), true);
+        assert_eq!(is_pal_u64(121), true);
+        assert_eq!(is_pal_u64(1231), false);
+        assert_eq!(is_pal_u64(777), true);
+        assert_eq!(is_pal_u64(1), true);
+        assert_eq!(is_pal_u64(100001), true);
+        assert_eq!(is_pal_u64(1221), true);
+        assert_eq!(is_pal_u64(134), false);
+    }
+
+    #[test]
+    fn test_is_pal_string_empty() {
+        assert!(is_pal_string("".to_string()));
+    }
+
+    #[test]
+    fn test_is_pal_string_ascii() {
+        assert!(is_pal_string("a".to_string()));
+        assert!(is_pal_string("aa".to_string()));
+        assert!(is_pal_string("aba".to_string()));
+        assert!(is_pal_string("abba".to_string()));
+        assert!(!is_pal_string("abc".to_string()));
+    }
+
+    #[test]
+    fn test_is_pal_string_unicode() {
+        assert!(is_pal_string("中".to_string()));
+        assert!(is_pal_string("中中".to_string()));
+        assert!(is_pal_string("a中a".to_string()));
+        assert!(is_pal_string("ab中ba".to_string()));
+        assert!(!is_pal_string("a中b".to_string()));
     }
 }

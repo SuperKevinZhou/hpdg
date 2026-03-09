b91268e327f2747df46f05b0109784559f5bb8f5
SuperKevinZhou <zzh__2020@outlook.com>
2026-03-09 21:52:00 +0800

    bindings: Python 绑定骨架

---
+++ bindings/python/lib.rs
@@ -0,0 +1,9 @@
+﻿#[cfg(feature = "python-bindings")]
+use pyo3::prelude::*;
+
+#[cfg(feature = "python-bindings")]
+#[pymodule]
+fn hpdg_py(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    let _ = m;
+    Ok(())
+}
